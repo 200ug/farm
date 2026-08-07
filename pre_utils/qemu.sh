5d4b1f771e4d11cdeb7f65ef97ab8af3380c2a63
2u <a@dyyni.org>
2026-08-08 00:08:05 +0300

    fix: libvirt+kvm group perms

--- pre_utils/qemu.sh
+++ pre_utils/qemu.sh
@@ -5,10 +5,7 @@
 
 sudo systemsudo apt update
 sudo apt install -y qemu-system libvirt-clients libvirt-daemon-system virtinst bridge-utils virt-manager
-
-sudo adduser $USER libvirt
-sudo adduser $USER kvm
-
+sudo usermod -aG libvirt $USER && sudo usermod -aG kvm $USER
 sudo systemctl enable --now libvirtd
 
 echo "[*] log out and back in (or reboot) for the group changes (libvirt & kvm) to take effect"
